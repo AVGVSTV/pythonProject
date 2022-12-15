18fafce936155778b818a70ef4495d5935a2a2ea
AVGVSTV <augustoxavier1989@gmail.com>
2022-12-15 13:40:52 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+#TASK NUMBER 1
+print('task number 1')
+
+a = 1
+b = 2.3
+c = 1+2j
+
+d = 'this is a string'
+
+x = 5 == 5
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(x)
+#TASK NUMBER 2
+print('task number 2')
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(x))
+
+
+print(a, (type(a)),  sep=' is type of ')
+print(b, (type(b)),  sep=' is type of ')
+print(c, (type(c)),  sep=' is type of ')
+print(d, (type(d)),  sep=' is type of ')
+print(x, (type(x)),  sep=' is type of ')
+
+#TASK NUMBER 3
+print('task number 3')
+
+print(isinstance(a, int))
+print(isinstance(b, int))
+print(isinstance(c, int))
+print(isinstance(d, int))
+print(isinstance(x, bool))
+
+#TASK NUMBER 4
+print('task number 4')
+
+
+
+print('Is', a, 'an instance of int?',isinstance(a, int))
+print('Is', b, 'an instance of bool?',isinstance(b, bool))
+print('Is', c, 'an instance of float?',isinstance(c, float))
+print('Is', x, 'an instance of list?',isinstance(x, list))
+
+
+
+#TASK NUMBER 5
+print('task number 5')
+
+'''
+This is a comment
+'''
+
+"""
+this is a comment with 2 citation marks
+"""
+# And that is also a comment with a hashtag
